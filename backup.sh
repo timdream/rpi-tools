d8e3f014b482f348eaa337a2c70c6f096904ab0d
Timothy Guan-tin Chien <timdream@gmail.com>
2019-06-09 23:44:23 -0700

    Use root rsync to backup

--- backup.sh
+++ backup.sh
@@ -3,5 +3,6 @@
 cd $(dirname $0) &&
 mkdir -p ./backup &&
 rsync -arvP -e "ssh  -o UserKnownHostsFile=./known_hosts" \
+   --rsync-path="sudo /usr/bin/rsync" \
   --files-from=rsync-files-from.txt \
   pi@raspberrypi.local:/ ./backup/
