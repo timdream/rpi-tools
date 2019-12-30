e0b2c27c3a1b14a2564e4b8c5a5e4e1d29f16ac1
Timothy Guan-tin Chien <timdream@gmail.com>
2019-12-29 23:17:33 -0800

    Print vpn script output to tty1

--- openvpn.sh
+++ openvpn.sh
@@ -18,46 +18,48 @@ sudo systemctl start openvpn@server-udp.service && \
 echo ">> Install /etc/cron.hourly/vpn" && \
 echo "#!/bin/bash
 
-echo ============================================== >> /var/log/vpn.log
-date >> /var/log/vpn.log
-echo ============================================== >> /var/log/vpn.log
+echo ============================================== | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+date | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+echo ============================================== | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 
 # Check interface
 __IFACE=\$(route -4 | grep default | awk '{print \$8}')
-printf \"Interface: %s\n\" \"\$__IFACE\" >> /var/log/vpn.log
+printf \"Interface: %s\n\" \"\$__IFACE\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 [ -z \"\$__IFACE\" ] && exit
 
 # Print the IP address
 __IP=\$(ifconfig \$__IFACE | grep \"inet \" | awk '{print \$2}')
-printf \"LAN IP: %s\n\" \"\$__IP\" >> /var/log/vpn.log
+printf \"LAN IP: %s\n\" \"\$__IP\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 [ -z \"\$__IP\" ] && exit
 
 # Print the external IP address
 __EXTERNAL_IP=\$(dig -4 @resolver1.opendns.com ANY myip.opendns.com +short)
-printf \"External IP: %s\n\" \"\$__EXTERNAL_IP\" >> /var/log/vpn.log
+printf \"External IP: %s\n\" \"\$__EXTERNAL_IP\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 [ -z \"\$__EXTERNAL_IP\" ] && exit
 
 # Dynamic DNS
 echo -n \"Dynamic DNS: \" >> /tmp/vpn.log
-curl -s \"https://dynamicdns.park-your-domain.com/update?host=_________&domain=____________&password=___________________\" >> /var/log/vpn.log
-echo >> /var/log/vpn.log
+curl -s \"https://dynamicdns.park-your-domain.com/update?host=_________&domain=____________&password=___________________\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+echo | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 
 # uPnP (SSH & OpenVPN UDP + TCP)
 if [[ -z \"\$(ssh-keyscan -p 8022 \$__EXTERNAL_IP 2>/dev/null)\" ]]; then
-  upnpc -d 8022 TCP >> /var/log/vpn.log
-  upnpc -d 28022 TCP >> /var/log/vpn.log
-  upnpc -d 443 TCP >> /var/log/vpn.log
-  upnpc -d 1194 UDP >> /var/log/vpn.log
+  upnpc -d 8022 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  upnpc -d 28022 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  upnpc -d 443 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  upnpc -d 1194 UDP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 
-  upnpc -a \"\$__IP\" 22 8022 TCP >> /var/log/vpn.log
-  upnpc -a \"\$__IP\" 22 28022 TCP >> /var/log/vpn.log
-  upnpc -r 1194 UDP 443 TCP >> /var/log/vpn.log
+  upnpc -a \"\$__IP\" 22 8022 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  upnpc -a \"\$__IP\" 22 28022 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  upnpc -r 1194 UDP 443 TCP | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 fi
 
 # OpenVPN
 if [ -z \"\$(iptables -t nat -L | grep 10.8.0.0)\" ]; then
-  echo \"Setup iptables\" >> /var/log/vpn.log
-  iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to \"\$__IP\" >> /var/log/vpn.log
+  echo \"Setup iptables\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
+  iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to \"\$__IP\" | sudo tee -a /dev/tty1 >> /var/log/vpn.log
 fi
+
+echo | sudo tee -a /dev/tty1
 " | sudo tee /etc/cron.hourly/vpn > /dev/null && \
 sudo chmod +x /etc/cron.hourly/vpn
